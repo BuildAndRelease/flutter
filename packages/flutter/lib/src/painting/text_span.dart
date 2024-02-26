fc54cfdca67c92380a9a97b31e1c59ddb7dfe273
yzh <yzhtracy@163.com>
2024-02-26 17:57:27 +0800

    fix: 输入框兼容一些异常表情

--- packages/flutter/lib/src/painting/text_span.dart
+++ packages/flutter/lib/src/painting/text_span.dart
@@ -277,7 +277,32 @@ class TextSpan extends InlineSpan implements HitTestTarget, MouseTrackerAnnotati
     }
     if (text != null) {
       try {
-        final t = text!.replaceAll('️⃣', '');
+        ///此处过滤掉会造成flutter异常的几个特殊字符
+        String t = text!.replaceAll('️⃣', '\u200b\u200b');
+        if (t.contains('\u200d\u2764\ufe0f\u200d')) {
+          RegExp pattern = RegExp(r'(.{2})\u200d\u2764\ufe0f\u200d(.{2})');
+          t = t.replaceAll(pattern, '\u200b\u200b\u200b💏\u200b\u200b\u200b');
+        }
+        t = t.replaceAll('‍♂️', '\u200b\u200b\u200b');
+        t = t.replaceAll('‍♀', '\u200b\u200b');
+        t = t.replaceAll('👩🏿‍🦯', '🦯\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('🧑‍🦯', '🦯\u200b\u200b\u200b');
+        t = t.replaceAll('👨‍🦯', '🦯\u200b\u200b\u200b');
+        t = t.replaceAll('👩🏽‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('👨🏼‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('🧑🏼‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('👩🏿‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('👨🏻‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('🧑🏽‍', '\u200b\u200b\u200b\u200b\u200b');
+        t = t.replaceAll('🕴️', '🧍\u200b');
+
+        if (t.contains('\ud83c') && t.contains('\ufe0f')) {
+          RegExp regex = RegExp(r'(\ud83c[\uDC00-\uDFFF]\ufe0f)');
+          t = t.replaceAllMapped(regex, (match) {
+            String emoji = match.group(0) ?? '';
+            return emoji.replaceFirst('\ufe0f', '\u200b');
+          });
+        }
         builder.addText(t);
       } on ArgumentError catch (exception, stack) {
         FlutterError.reportError(FlutterErrorDetails(
